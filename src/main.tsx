9564a5d06abf9ecc2b43eca7d56fa2a9517761e9
CrushLineCo <crushlineco@gmail.com>
2025-08-09 06:41:40 +0000

    Configure Amplify at bootstrap; ignore amplify_outputs.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './index.css'
 
+import outputs from '../amplify_outputs.json' // adjust path if amplify_outputs.json is at repo root
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure(outputs)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
